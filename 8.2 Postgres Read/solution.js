5534137e5f7c34a26af082778ba56a5aab6e41ce
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-05-15 10:46:34 +0100

    Add flag quiz application with Express, PostgreSQL, and EJS
    - Implemented main CSS styles for the quiz interface.
    - Created solution.js to handle server logic, database connection, and quiz functionality.
    - Developed index.ejs for rendering the quiz page, including score tracking and user input.
    - Added functionality to fetch random quiz questions from the database and display them.
    - Included user feedback for correct/incorrect answers and a restart option.

--- 8.2 Postgres Read/solution.js
+++ 8.2 Postgres Read/solution.js
@@ -6,7 +6,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "Meedaah19",
+  password: "123456",
   port: 5432,
 });
 
@@ -16,7 +16,7 @@ const port = 3000;
 db.connect();
 
 let quiz = [];
-db.query("SELECT * FROM capitals", (err, res) => {
+db.query("SELECT * FROM flags", (err, res) => {
   if (err) {
     console.error("Error executing query", err.stack);
   } else {
@@ -45,7 +45,7 @@ app.get("/", async (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -61,6 +61,7 @@ app.post("/submit", (req, res) => {
 
 async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
   currentQuestion = randomCountry;
 }
 
