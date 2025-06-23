075a4f1d414dac1f2099adddcef23bb5be2b5e60
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-06-23 17:48:09 +0100

    Refactor authentication code to use environment variables and hash passwords

--- 9.2+Authentication+Lv.2/index.js
+++ 9.2+Authentication+Lv.2/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,12 +47,18 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log('Error hashing password::', err)
+        } else{
+         const result = await db.query(
+          "INSERT INTO users (email, password) VALUES ($1, $2)",
+          [email, hash]
+        );
+        console.log(result);
+        res.render("secrets.ejs");
+        }
+      })
     }
   } catch (err) {
     console.log(err);
