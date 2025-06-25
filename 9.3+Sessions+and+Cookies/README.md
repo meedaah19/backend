b79d684cd4e5c4e83b00a3f58149f5f0ba25d957
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-06-25 18:31:43 +0100

    feat: Initialize project with environment variables and user authentication
    - Added package.json with necessary dependencies for Express, Passport, and PostgreSQL.
    - Created EJS partials for header and footer.
    - Developed main application logic in solution.js for user registration, login, and session management.
    - Implemented views for home, login, register, and secrets pages with Bootstrap styling.
    - Styled application with custom CSS for better user experience.

--- 9.3+Sessions+and+Cookies/README.md
+++ 9.3+Sessions+and+Cookies/README.md
@@ -3,3 +3,7 @@ bcrypt/ compare is use to compare the password login and the password in nthe da
 cookies are use widely on the internet to save browsing section, beyound saving your last section.
 
 A section is the amount of time a user interact with the internet.
+
+the password line of code must come after the session line of code
+
+to set timer to the time your login will expire you will use cookies then set timer
--- 9.3+Sessions+and+Cookies/index.js
+++ 9.3+Sessions+and+Cookies/index.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,18 @@ const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(session({
+  secret:  'TOPSECRETWORD',
+  resave: false,
+  saveUninitialized: true,
+  cookies: {
+    maxAge: 1000 * 60 * 24
+  }
+}));
+
+app.use (passport.initialize());
+app.use(passport.session());
 
 const db = new pg.Client({
    user: process.env.DB_USER,
@@ -33,6 +47,14 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
+app.get('/secrets',  (req, res) =>{
+  if(req.isAuthenticated()) {
+    res.render('secrets.ejs')
+  } else {
+    res.redirect('/login');
+  }
+})
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -56,7 +78,11 @@ app.post("/register", async (req, res) => {
             "INSERT INTO users (email, password) VALUES ($1, $2)",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          const user = results.rows[0];
+          req.login(user, (err) => {
+            console.log(err)
+            res.redirect('/secrets')
+          })
         }
       });
     }
@@ -65,10 +91,12 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
+app.post("/login", passport.authenticate('local',{
+  successRedirect: '/secrets',
+  failureRedirect: '/login',
+}));
 
+passport.use(new Strategy(async function verily(email, password, cb) {
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
       email,
@@ -76,24 +104,32 @@ app.post("/login", async (req, res) => {
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          return cb(err)
         } else {
           if (result) {
-            res.render("secrets.ejs");
+            return cb(null, user)
           } else {
-            res.send("Incorrect Password");
+            return cb(null, false)
           }
         }
       });
     } else {
-      res.send("User not found");
+      return cb('User not found')
     }
   } catch (err) {
     console.log(err);
   }
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
 });
+ 
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
